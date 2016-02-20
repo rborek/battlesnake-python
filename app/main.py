8f2d0e6747d975d8c214ed167b2400088ec693e4
Ryan Borek <ryanborek.ca@gmail.com>
2016-02-20 14:54:55 -0800

    added utility functions

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 import bottle
 import os
 
-id = "c6c28e7d-0f7e-473c-a2bc-8ee6dfc4a1a2";
+id = "c6c28e7d-0f7e-473c-a2bc-8ee6dfc4a1a2"
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -31,13 +31,42 @@ def start():
         'taunt': 'battlesnake-python!'
     }
 
+def getEmptyGrid():
+    return [[0 for row in range(data['width'])] for col in range(data['height'])];
+
+def getMe(data):
+    for snake in data['snakes']:
+        if snake['id'] == id:
+            return snake
+
+def getGrid(data):
+    grid = getEmptyGrid()
+    for snake in data['snakes']:
+        for coord in snake['coords']:
+            grid[coord[0]][coord[1]] = 1
+    return grid
+
+def getGridOfPossibleMovesByOtherSnakes(data):
+    grid = getEmptyGrid()
+    for snake in data['snakes']
+        if snake['id'] != id:
+            for coord in snake['coords']:
+                grid[coord[0]][coord[1]] = 1
+    return grid
+
+def getSafeMovesBasedOnOtherSnakes(data):
+    grid = getGridOfPossibleMovesByOtherSnakes(data)
+    me = getMe(data)
+    head = me['coords'][0]
+    safeMoves = []
+    return safeMoves
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    grid = [[0 for i in xrange(data['width'])] for i in xrange(data['height'])]
-    toMove = 'north';
-    head = [0, 0];
+    grid = getGrid(data) 
+    toMove = 'north'
+    head = [0, 0]
     for snake in data['snakes']:
         # if it is us
         if snake['id'] == id:
@@ -50,8 +79,6 @@ def move():
                 toMove = 'east'
             elif head[1] == data['height'] - 1:
                 toMove = 'west'
-
-
 
     # TODO: Do things with data
 
